10f5bd52554d936dcf95b025992fab13061d84c1
Anjali <anjali.ep@gmail.com>
2016-09-30 23:00:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-1', function (req, res) {
+  res.send('Article one requested');
+});
+
+app.get('/article-2', function (req, res) {
+  res.send('Article two requested');
+});
+
+app.get('/article-3', function (req, res) {
+  res.send('Article three requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
